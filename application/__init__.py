ae1bd581ce1f3ff8cbf4f7ba7079c36475954b2c
Brian <brianvoo@gmail.com>
2022-12-15 12:23:04 +0800

    assets fix

--- application/__init__.py
+++ application/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_redis import FlaskRedis
-from flask_assets import Environment
-from .assets import compile_assets
+# from flask_assets import Environment
+# from .assets import compile_assets
 
 
 
 # Globally accessible libraries
 db = SQLAlchemy()
 r = FlaskRedis()
-assets = Environment()
+# assets = Environment()
 
 def init_app():
     """Initialize the core application."""
@@ -19,11 +19,11 @@ def init_app():
     # Initialize Plugins
     db.init_app(app)
     r.init_app(app)
-    assets.init_app(app)
+    # assets.init_app(app)
     
 
     with app.app_context():
-        db.create_all()
+        # db.create_all()
         
         # Include Routes
         from .home import routes
@@ -39,6 +39,6 @@ def init_app():
         app.register_blueprint(about.routes.about_bp)
         app.register_blueprint(contact.routes.contact_bp)
 
-        compile_assets(assets)
+        # compile_assets(assets)
 
         return app
--- application/assets.py
+++ application/assets.py
@@ -1,19 +1,19 @@
-"""Compile static assets."""
-from flask import current_app as app
-from flask_assets import Bundle
+# """Compile static assets."""
+# from flask import current_app as app
+# from flask_assets import Bundle
 
-def compile_assets(assets):
-    """Configure and build asset bundles."""
+# def compile_assets(assets):
+#     """Configure and build asset bundles."""
 
-    #Home assets bundle
-    home_style_bundle = Bundle(
-        'src/less/*.less',
-        'home_bp/home.less',
-        filters='less,cssmin',
-        output='dist/css/home.css',
-        extra={'rel': 'stylesheet/css'}
-    )
+#     #Home assets bundle
+#     home_style_bundle = Bundle(
+#         'src/less/*.less',
+#         'home_bp/home.less',
+#         filters='less,cssmin',
+#         output='dist/css/home.css',
+#         extra={'rel': 'stylesheet/css'}
+#     )
     
-    assets.register('home_styles', home_style_bundle)
-    if app.config['FLASK_ENV'] == 'development':
-        home_style_bundle.build()
+#     assets.register('home_styles', home_style_bundle)
+#     if app.config['FLASK_ENV'] == 'development':
+#         home_style_bundle.build()
